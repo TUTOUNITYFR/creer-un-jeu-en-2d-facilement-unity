49aebc6ca73e7257d2775ce33d921c1cbf6e1c1e
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-02-08 17:00:38 +0100

    Épisode 4 : Premier ennemi

---
+++ Assets/EnemyPatrol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // Si l'ennemi est quasiment arrivé à sa destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
