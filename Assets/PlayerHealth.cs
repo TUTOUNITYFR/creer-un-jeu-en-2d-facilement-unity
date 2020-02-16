cc4aa3aa57ef7387a677cd15402b1d0a74a0f43c
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-02-16 17:08:02 +0100

    Épisode 5 : Ajout de la barre de vie

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
