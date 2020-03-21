5e5c708ab41586eb06538ba8628680462b57a966
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-03-21 19:08:11 +0100

    Épisode 8 : Pièces

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+    public Text coinsCountText;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de Inventory dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    public void AddCoins(int count)
+    {
+        coinsCount += count;
+        coinsCountText.text = coinsCount.ToString();
+    }
+}
---
+++ Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Inventory.instance.AddCoins(1);
+            Destroy(gameObject);
+        }
+    }
+}
