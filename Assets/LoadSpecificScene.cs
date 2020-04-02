0f0615bc008d4f213a61606f356ae4c4f68f24bb
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-04-02 16:14:55 +0200

    Épisode 9 : changement de niveau

---
+++ Assets/LoadSpecificScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string sceneName;
+    public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine(loadNextScene());
+        }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        fadeSystem.SetTrigger("FadeIn");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
