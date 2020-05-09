e05e5069ce13093e12dbe1f6deb547c6b0212d81
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-05-09 14:41:06 +0200

    Épisode 14 : Recupérer de la vie

---
+++ Assets/HealPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HealPowerUp : MonoBehaviour
+{
+    public int healthPoints;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(PlayerHealth.instance.currentHealth != PlayerHealth.instance.maxHealth)
+            {
+                PlayerHealth.instance.HealPlayer(healthPoints);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -13,6 +13,19 @@ public class PlayerHealth : MonoBehaviour
     public SpriteRenderer graphics;
     public HealthBar healthBar;
 
+    public static PlayerHealth instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de PlayerHealth dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -25,6 +38,20 @@ public class PlayerHealth : MonoBehaviour
         {
             TakeDamage(20);
         }
+    }
+
+    public void HealPlayer(int amount)
+    {
+        if((currentHealth + amount) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += amount;
+        }
+
+        healthBar.SetHealth(currentHealth);
     }
 
     public void TakeDamage(int damage)
