e36113c80a572793328723b56af7d507487868af
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-08-05 09:46:14 +0200

    Épisode 24

--- Assets/Scripts/Ladder.cs
+++ Assets/Scripts/Ladder.cs
@@ -6,7 +6,7 @@ public class Ladder : MonoBehaviour
     private bool isInRange;
     private PlayerMovement playerMovement;
     public BoxCollider2D topCollider;
-    public Text interactUI;
+    private Text interactUI;
 
     void Awake()
     {
