6386e86e0b22d200687a57564e11d23fc62ec907
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-08-13 09:37:38 +0200

    Épisode 27 : Garder les données entre les niveaux

---
+++ Assets/LoadAndSaveData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class LoadAndSaveData : MonoBehaviour
+{
+    public static LoadAndSaveData instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de LoadAndSaveData dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+
+    void Start()
+    {
+        Inventory.instance.coinsCount = PlayerPrefs.GetInt("coinsCount", 0);
+        Inventory.instance.UpdateTextUI();
+
+        /*int currentHealth = PlayerPrefs.GetInt("playerHealth", PlayerHealth.instance.maxHealth);
+        PlayerHealth.instance.currentHealth = currentHealth;
+        PlayerHealth.instance.healthBar.SetHealth(currentHealth);*/
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("coinsCount", Inventory.instance.coinsCount);
+        //PlayerPrefs.SetInt("playerHealth", PlayerHealth.instance.currentHealth);
+    }
+}
