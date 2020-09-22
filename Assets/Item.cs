139e0f3ba629e79e9855704d6f8a8f309f077733
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-09-22 13:46:42 +0200

    Épisode 31 et 32 : Inventaire du joueur

---
+++ Assets/Item.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string name;
+    public string description;
+    public Sprite image;
+    public int hpGiven;
+    public int speedGiven;
+    public float speedDuration;
+}
--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     public int coinsCount;
     public Text coinsCountText;
+
+    public List<Item> content = new List<Item>();
+    public int contentCurrentIndex = 0;
+    public Image itemImageUI;
+    public Text itemNameUI;
+    public Sprite emptyItemImage;
 
     public static Inventory instance;
 
@@ -17,6 +24,71 @@ public class Inventory : MonoBehaviour
         }
 
         instance = this;
+    }
+
+    private void Start()
+    {
+        UpdateInventoryUI();
+    }
+
+    public void ConsumeItem()
+    {
+        if(content.Count == 0)
+        {
+            return;
+        }
+
+        Item currentItem = content[contentCurrentIndex];
+        PlayerHealth.instance.HealPlayer(currentItem.hpGiven);
+        PlayerMovement.instance.moveSpeed += currentItem.speedGiven;
+        content.Remove(currentItem);
+        GetNextItem();
+        UpdateInventoryUI();
+    }
+
+    public void GetNextItem()
+    {
+        if (content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex++;
+        if(contentCurrentIndex > content.Count - 1)
+        {
+            contentCurrentIndex = 0;
+        }
+        UpdateInventoryUI();
+    }
+
+    public void GetPreviousItem()
+    {
+        if(content.Count == 0)
+        {
+            return;
+        }
+
+        contentCurrentIndex--;
+        if(contentCurrentIndex < 0)
+        {
+            contentCurrentIndex = content.Count - 1;
+        }
+        UpdateInventoryUI();
+    }
+
+    public void UpdateInventoryUI()
+    {
+        if(content.Count > 0)
+        {
+            itemImageUI.sprite = content[contentCurrentIndex].image;
+            itemNameUI.text = content[contentCurrentIndex].name;
+        }
+        else
+        {
+            itemImageUI.sprite = emptyItemImage;
+            itemNameUI.text = "";
+        }
+        
     }
 
     public void AddCoins(int count)
