5d715db73b0aea6fa2d80b3001be1e8c07696b4a
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-10-25 12:01:04 +0100

    Episode 36 : Sauvegarder les items

---
+++ Assets/ItemsDatabase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ItemsDatabase : MonoBehaviour
+{
+    public Item[] allItems;
+
+    public static ItemsDatabase instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de ItemsDatabase dans la scène");
+            return;
+        }
+
+        instance = this;
+    }
+}
